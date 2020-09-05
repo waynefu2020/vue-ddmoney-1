6ccb336fb84525b6ddf9e6ae2e0227f7c6da3b64
ddwayne <652507565@qq.com>
2020-09-05 16:14:03 +0800

    引入全局组件<Nav/>

---
+++ src/components/Nav.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <router-link to="/money">记账</router-link>
+        丨<router-link to="/labels">标签</router-link>
+        丨<router-link to="/statistics">统计</router-link>
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+        name:'Nav'
+    };
+</script>
+
+<style lang="scss" scoped>
+
+</style>

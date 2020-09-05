744d20e64b4a3a3a3a7e2b126b781e0b9ee7af77
ddwayne <652507565@qq.com>
2020-09-05 16:34:21 +0800

    添加404页面

---
+++ src/views/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>$END$</div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue';
+    import {Component} from 'vue-property-decorator';
+
+    @Component
+    export default class NotFound extends Vue {
+
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>

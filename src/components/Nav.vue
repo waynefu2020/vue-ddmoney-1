acc027ca15b82d5b7ab755cc1b92fd4b8482ac14
ddwayne <652507565@qq.com>
2020-09-06 15:51:43 +0800

    完成Money的CSS

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -24,8 +24,8 @@
 <style lang="scss" scoped>
     @import "~@/assets/style/helper.scss";
     nav {
+        @extend %outerShadow;
         display: flex;
-        box-shadow: 0 0 3px rgba(0,0,0,0.1);
         flex-direction: row;
         font-size: 12px;
         >.item {

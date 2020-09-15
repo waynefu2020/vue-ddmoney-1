b3fcbbbcf4d9858933013c7c647854ccd7e47e62
ddwayne <652507565@qq.com>
2020-09-15 22:51:31 +0800

    抽离 Tabs.vue 并应用到 Statistics.vue 中

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -1,34 +1,41 @@
 <template>
-    <div>
-        <ul class="types">
-            <li :class="{[classPrefix+'-item']: classPrefix, selected: value === '-'}"
-                @click="selectType('-')">支出</li>
-            <li :class="{[classPrefix + '-item']: classPrefix, selected: value === '+'}"
-                @click="selectType('+')">收入</li>
-        </ul>
-    </div>
+    <ul class="tabs">
+        <li v-for="item in dataSource" :key="item.value"
+            :class="liClass(item)"
+            @click="select(item)"
+        >{{item.text}}</li>
+    </ul>
 </template>
 
 <script lang="ts">
     import Vue from 'vue';
     import {Component, Prop} from 'vue-property-decorator';
 
-    @Component
-    export default class Types extends Vue {
-        @Prop(String) readonly value!: string;
-        @Prop(String) readonly classPrefix?: string;
+    type DataSourceItem = {text: string; value: string}
 
-        selectType(type: string){
-            if(type !== '-' && type !== '+'){
-                throw new Error('type is wrong')
+    @Component
+    export default class Tabs extends Vue {
+        @Prop({required: true, type: Array})
+        dataSource!: DataSourceItem[];
+        @Prop(String)
+        value!: string;
+        @Prop(String)
+        classPrefix?: string;
+
+        liClass(item: DataSourceItem){
+            return {
+                [this.classPrefix+'-tabs-item']: this.classPrefix, selected: item.value === this.value
             }
-            this.$emit('update:value', type)
+        }
+
+        select(item: DataSourceItem) {
+            this.$emit('update:value', item.value)
         }
     }
 </script>
 
 <style lang="scss" scoped>
-    .types{
+    .tabs{
         background: #c4c4c4;
         display: flex;
         align-items: center;
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,17 +1,48 @@
 <template>
     <div>
         <Layout>
-            <p>Statistics</p>
+            <Types class-prefix="type" :value.sync="type" :data-source="typeList"/>
+            <Tabs class-prefix="interval" :value.sync="interval" :data-source="intervalList"/>
+            <div>
+                type: {{type}}
+                interval: {{interval}}
+            </div>
         </Layout>
     </div>
 </template>
 
+<style lang="scss" scoped>
+    ::v-deep .type-tabs-item{
+        background: white;
+        &.selected{
+            background: #bcbcbc;
+            &::after{
+                display: none;
+            }
+        }
+    }
+</style>
+
 <script lang="ts">
-    export default {
-        name: "Statistics",
+    import Vue from 'vue';
+    import Types from '@/components/Money/Types.vue';
+    import {Component} from 'vue-property-decorator';
+    import Tabs from '@/components/Tabs.vue';
+
+    @Component({
+        components:{Tabs, Types}
+    })
+    export default class Statistics extends Vue{
+        type = '-';
+        interval = 'day';
+        intervalList = [
+            {text:'天', value:'day'},
+            {text:'周', value:'week'},
+            {text:'月', value:'month'}
+        ];
+        typeList = [
+            {text:'支出', value:'-'},
+            {text:'收入', value:'+'},
+        ]
     }
 </script>
-
-<style lang="scss" scoped>
-
-</style>
